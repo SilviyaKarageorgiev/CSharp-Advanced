f20fd6a8cf143de7299fd1581e91eca5c0447d4c
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2022-04-11 22:34:35 +0300

    Add solutions from Multidimensional Arrays Lab

---
+++ 02. Multidimensional Arrays/Multidimensional Arrays - Lab/P01.SumMatrixElements/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace P01.SumMatrixElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputRowsCols = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int rows = inputRowsCols[0];
+            int cols = inputRowsCols[1];
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+
+                    matrix[row, col] = input[col];
+                }
+            }
+            int sum = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    sum += matrix[row, col];
+                }
+
+            }
+            Console.WriteLine(rows);
+            Console.WriteLine(cols);
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ 02. Multidimensional Arrays/Multidimensional Arrays - Lab/P02.SumMatrixColumns/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace P02.SumMatrixColumns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] inputRowsCols = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int rows = inputRowsCols[0];
+            int cols = inputRowsCols[1];
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+
+            int currSum = 0;
+            for (int coll = 0; coll < matrix.GetLength(1); coll++)
+            {
+
+                for (int roww = 0; roww < matrix.GetLength(0); roww++)
+                {
+                    currSum += matrix[roww, coll];
+                }
+                Console.WriteLine(currSum);
+                currSum = 0;
+            }
+
+        }
+    }
+}
