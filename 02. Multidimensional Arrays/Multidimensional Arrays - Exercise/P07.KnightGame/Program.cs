d25533fc8b839d8faec8632b8f9ef00e50675d5f
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2022-04-14 13:40:21 +0300

    Add two solutions

---
+++ 02. Multidimensional Arrays/Multidimensional Arrays - Exercise/P07.KnightGame/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Linq;
+
+namespace P07.KnightGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] board = new char[n, n];
+
+            for (int row = 0; row < board.GetLength(0); row++)
+            {
+                string input = Console.ReadLine();
+
+                for (int col = 0; col < board.GetLength(1); col++)
+                {
+                    board[row, col] = input[col];
+                }
+            }
+
+            int maxCount = 0;
+            int currCount = 0;
+            int maxCountRow = 0;
+            int maxCountCol = 0;
+            int removedKnights = 0;
+
+            while (true)
+            {
+                for (int row = 0; row < board.GetLength(0); row++)
+                {
+
+                    for (int col = 0; col < board.GetLength(1); col++)
+                    {
+
+                        if (board[row, col] == '0')
+                        {
+                            continue;
+                        }
+
+                        //up
+                        if (row - 2 >= 0 && row - 2 < board.GetLength(0))
+                        {
+                            if (col - 1 >= 0 && col - 1 < board.GetLength(1))
+                            {
+                                if (board[row - 2, col - 1] == 'K')
+                                {
+                                    currCount++;
+                                }
+                            }
+                            if (col + 1 >= 0 && col + 1 < board.GetLength(1))
+                            {
+                                if (board[row - 2, col + 1] == 'K')
+                                {
+                                    currCount++;
+                                }
+                            }
+                        }
+
+                        //down
+                        if (row + 2 >= 0 && row + 2 < board.GetLength(0))
+                        {
+                            if (col - 1 >= 0 && col - 1 < board.GetLength(1))
+                            {
+                                if (board[row + 2, col - 1] == 'K')
+                                {
+                                    currCount++;
+                                }
+                            }
+                            if (col + 1 >= 0 && col + 1 < board.GetLength(1))
+                            {
+                                if (board[row + 2, col + 1] == 'K')
+                                {
+                                    currCount++;
+                                }
+                            }
+                        }
+
+                        //left
+                        if (col - 2 >= 0 && col - 2 < board.GetLength(1))
+                        {
+                            if (row - 1 >= 0 && row - 1 < board.GetLength(0))
+                            {
+                                if (board[row - 1, col - 2] == 'K')
+                                {
+                                    currCount++;
+                                }
+                            }
+                            if (row + 1 >= 0 && row + 1 < board.GetLength(0))
+                            {
+                                if (board[row + 1, col - 2] == 'K')
+                                {
+                                    currCount++;
+                                }
+                            }
+                        }
+
+                        //right
+                        if (col + 2 >= 0 && col + 2 < board.GetLength(1))
+                        {
+                            if (row - 1 >= 0 && row - 1 < board.GetLength(0))
+                            {
+                                if (board[row - 1, col + 2] == 'K')
+                                {
+                                    currCount++;
+                                }
+                            }
+                            if (row + 1 >= 0 && row + 1 < board.GetLength(0))
+                            {
+                                if (board[row + 1, col + 2] == 'K')
+                                {
+                                    currCount++;
+                                }
+                            }
+                        }
+
+                        if (currCount > maxCount)
+                        {
+                            maxCount = currCount;
+                            maxCountRow = row;
+                            maxCountCol = col;
+                        }
+                        currCount = 0;
+                    }
+                }
+
+                if (maxCount > 0)
+                {
+                    board[maxCountRow, maxCountCol] = '0';
+                    removedKnights++;
+                    maxCount = 0;
+                    currCount = 0;
+                }
+                else
+                {
+                    Console.WriteLine(removedKnights);
+                    break;
+                }
+            }
+
+        }
+    }
+}
---
+++ 02. Multidimensional Arrays/Multidimensional Arrays - Exercise/P08.Bombs/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Linq;
+
+namespace P08.Bombs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] inputMatrix = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = inputMatrix[col];
+                }
+            }
+
+            string[] inputBombs = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            for (int i = 0; i < inputBombs.Length; i++)
+            {
+                int[] currBomb = inputBombs[i].Split(",").Select(int.Parse).ToArray();
+                int rowIndex = currBomb[0];
+                int colIndex = currBomb[1];
+
+                if (matrix[rowIndex, colIndex] <= 0)
+                {
+                    continue;
+                }
+
+                int bombPower = matrix[rowIndex, colIndex];
+                matrix[rowIndex, colIndex] = 0;
+
+                if (rowIndex - 1 >= 0 && rowIndex - 1 < matrix.GetLength(0) && colIndex >= 0 && colIndex < matrix.GetLength(0))
+                {
+                    if (matrix[rowIndex - 1, colIndex] > 0)
+                    {
+                        matrix[rowIndex - 1, colIndex] -= bombPower;
+                    }
+                }
+
+                if (rowIndex - 1 >= 0 && rowIndex - 1 < matrix.GetLength(0) && colIndex - 1 >= 0 && colIndex - 1 < matrix.GetLength(0))
+                {
+                    if (matrix[rowIndex - 1, colIndex - 1] > 0)
+                    {
+                        matrix[rowIndex - 1, colIndex - 1] -= bombPower;
+                    }
+                }
+
+                if (rowIndex >= 0 && rowIndex < matrix.GetLength(0) && colIndex - 1 >= 0 && colIndex - 1 < matrix.GetLength(0))
+                {
+                    if (matrix[rowIndex, colIndex - 1] > 0)
+                    {
+                        matrix[rowIndex, colIndex - 1] -= bombPower;
+                    }
+                }
+
+                if (rowIndex + 1 >= 0 && rowIndex + 1 < matrix.GetLength(0) && colIndex - 1 >= 0 && colIndex - 1 < matrix.GetLength(0))
+                {
+                    if (matrix[rowIndex + 1, colIndex - 1] > 0)
+                    {
+                        matrix[rowIndex + 1, colIndex - 1] -= bombPower;
+                    }
+                }
+
+                if (rowIndex + 1 >= 0 && rowIndex + 1 < matrix.GetLength(0) && colIndex >= 0 && colIndex < matrix.GetLength(0))
+                {
+                    if (matrix[rowIndex + 1, colIndex] > 0)
+                    {
+                        matrix[rowIndex + 1, colIndex] -= bombPower;
+                    }
+                }
+
+                if (rowIndex + 1 >= 0 && rowIndex + 1 < matrix.GetLength(0) && colIndex + 1 >= 0 && colIndex + 1 < matrix.GetLength(0))
+                {
+                    if (matrix[rowIndex + 1, colIndex + 1] > 0)
+                    {
+                        matrix[rowIndex + 1, colIndex + 1] -= bombPower;
+                    }
+                }
+
+                if (rowIndex >= 0 && rowIndex < matrix.GetLength(0) && colIndex + 1 >= 0 && colIndex + 1 < matrix.GetLength(0))
+                {
+                    if (matrix[rowIndex, colIndex + 1] > 0)
+                    {
+                        matrix[rowIndex, colIndex + 1] -= bombPower;
+                    }
+                }
+
+                if (rowIndex - 1 >= 0 && rowIndex - 1 < matrix.GetLength(0) && colIndex + 1 >= 0 && colIndex + 1 < matrix.GetLength(0))
+                {
+                    if (matrix[rowIndex - 1, colIndex + 1] > 0)
+                    {
+                        matrix[rowIndex - 1, colIndex + 1] -= bombPower;
+                    }
+                }
+                
+            }
+
+            int activeCells = 0;
+            int sumActiveCells = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] > 0)
+                    {
+                        activeCells++;
+                        sumActiveCells += matrix[row, col];
+                    }
+                }
+            }
+
+            Console.WriteLine($"Alive cells: {activeCells}");
+            Console.WriteLine($"Sum: {sumActiveCells}");
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write($"{matrix[row, col]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
