3233ced7d3d2f743bab505bc12e7e3f60b02a8bc
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2022-04-19 22:33:43 +0300

    Add two solutions

---
+++ 03. Sets and Dictionaries Advanced/Sets and Dictionaries Advanced - Exercise/P06.Wardrobe/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P06.Wardrobe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, Dictionary<string, int>> wardrobe = new Dictionary<string, Dictionary<string, int>>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(" -> ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                string color = input[0];
+                string[] clothes = input[1].Split(",", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                if (!wardrobe.ContainsKey(color))
+                {
+                    wardrobe.Add(color, new Dictionary<string, int>());
+                }
+
+                foreach (var item in clothes)
+                {
+                    if (!wardrobe[color].ContainsKey(item))
+                    {
+                        wardrobe[color].Add(item, 0);
+                    }
+                    wardrobe[color][item]++;
+                }
+            }
+
+            string[] wantedClothe = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            string colorOfWantedClothe = wantedClothe[0];
+            string typeOfWantedClothe = wantedClothe[1];
+
+            foreach (var color in wardrobe)
+            {
+                Console.WriteLine($"{color.Key} clothes:");
+
+                foreach (var (clothe, count) in color.Value)
+                {
+                    if (color.Key == colorOfWantedClothe && clothe
+                        == typeOfWantedClothe)
+                    {
+                        Console.WriteLine($"* {clothe} - {count} (found!)");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"* {clothe} - {count}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 03. Sets and Dictionaries Advanced/Sets and Dictionaries Advanced - Exercise/P07.TheVLogger/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P07.TheVLogger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, Dictionary<string, HashSet<string>>> data = new Dictionary<string, Dictionary<string, HashSet<string>>>();
+
+            string input;
+            while ((input = Console.ReadLine()) != "Statistics")
+            {
+                string[] cmdArgs = input.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                string firstVlogger = cmdArgs[0];
+                string command = cmdArgs[1];
+
+                if (command == "joined")
+                {
+                    if (!data.ContainsKey(firstVlogger))
+                    {
+                        data[firstVlogger] = new Dictionary<string, HashSet<string>>();
+                        data[firstVlogger].Add("following", new HashSet<string>());
+                        data[firstVlogger].Add("followers", new HashSet<string>());
+                    }
+                }
+
+                else if (command == "followed")
+                {
+                    string secondVlogger = cmdArgs[2];
+                    if (firstVlogger == secondVlogger)
+                    {
+                        continue;
+                    }
+
+                    if (!data.ContainsKey(secondVlogger) || !data.ContainsKey(firstVlogger))
+                    {
+                        continue;
+                    }
+
+                    if (!data[firstVlogger]["following"].Contains(secondVlogger) && !data[secondVlogger]["followers"].Contains(firstVlogger))
+                    {
+                        data[secondVlogger]["followers"].Add(firstVlogger);
+                        data[firstVlogger]["following"].Add(secondVlogger);
+                    }
+                }
+            }
+
+            Console.WriteLine($"The V - Logger has a total of {data.Count} vloggers in its logs.");
+
+            var sortedVlogers = data.OrderByDescending(x => x.Value["followers"].Count).ThenBy(x => x.Value["following"].Count);
+
+            int counter = 1;
+            foreach (var currVlogger in sortedVlogers)
+            {
+                Console.WriteLine($"{counter}. {currVlogger.Key} : {currVlogger.Value["followers"].Count} followers, {currVlogger.Value["following"].Count} following");
+
+                if (counter == 1)
+                {
+                    foreach (var item in currVlogger.Value["followers"].OrderBy(x => x))
+                    {
+                        Console.WriteLine($"*  {item}");
+                    }
+                }
+
+                counter++;
+            }
+        }
+    }
+}
