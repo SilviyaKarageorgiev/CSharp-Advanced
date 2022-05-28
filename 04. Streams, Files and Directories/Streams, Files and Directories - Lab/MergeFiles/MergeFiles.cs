572e1d09eb0a6f92196a22ff9b385a215df8ca22
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2022-05-28 14:56:13 +0300

    Add a solution

---
+++ 04. Streams, Files and Directories/Streams, Files and Directories - Lab/MergeFiles/MergeFiles.cs
@@ -0,0 +1,44 @@
+﻿namespace MergeFiles
+{
+    using System;
+    using System.IO;
+    public class MergeFiles
+    {
+        static void Main(string[] args)
+        {
+            var firstInputFilePath = @"..\..\..\Files\input1.txt";
+            var secondInputFilePath = @"..\..\..\Files\input2.txt";
+            var outputFilePath = @"..\..\..\Files\output.txt";
+
+            MergeTextFiles(firstInputFilePath, secondInputFilePath, outputFilePath);
+        }
+
+        public static void MergeTextFiles(string firstInputFilePath, string secondInputFilePath, string outputFilePath)
+        {
+            StreamReader reader1 = new StreamReader(firstInputFilePath);
+            StreamReader reader2 = new StreamReader(secondInputFilePath);
+            StreamWriter writer = new StreamWriter(outputFilePath);
+
+            using (reader1)
+            {
+                using (reader2)
+                {
+                    using (writer)
+                    {
+                        while (!reader1.EndOfStream || !reader2.EndOfStream)
+                        {
+                            if (reader1 != null)
+                            {
+                                writer.WriteLine(reader1.ReadLine());
+                            }
+                            if (reader2 != null)
+                            {
+                                writer.WriteLine(reader2.ReadLine());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
