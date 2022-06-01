9e29e5e59dbd4ac79ef5ff6fc81a6c016a3edf94
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2022-06-01 14:51:34 +0300

    Add solutions

---
+++ 04. Streams, Files and Directories/Streams, Files and Directories - Exercise/CopyBinaryFile/CopyBinaryFile.cs
@@ -0,0 +1,39 @@
+﻿namespace CopyBinaryFile
+{
+    using System;
+    using System.IO;
+
+    public class CopyBinaryFile
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\copyMe.png";
+            string outputFilePath = @"..\..\..\copyMe-copy.png";
+
+            CopyFile(inputFilePath, outputFilePath);
+        }
+
+        public static void CopyFile(string inputFilePath, string outputFilePath)
+        {
+            
+            using (FileStream reader = new FileStream(inputFilePath, FileMode.Open))
+            {
+                
+                using (FileStream writer = new FileStream(outputFilePath, FileMode.Create))
+                {
+                    while (true)
+                    {
+                        byte[] buffer = new byte[4096];
+                        int countBytes = reader.Read(buffer, 0, buffer.Length);
+                        if (countBytes == 0)
+                        {
+                            break;
+                        }
+
+                        writer.Write(buffer, 0, countBytes);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 04. Streams, Files and Directories/Streams, Files and Directories - Exercise/CopyDirectory/CopyDirectory.cs
@@ -0,0 +1,36 @@
+﻿namespace CopyDirectory
+{
+    using System;
+    using System.IO;
+
+    public class CopyDirectory
+    {
+        static void Main()
+        {
+            string inputPath = @$"{Console.ReadLine()}";
+            string outputPath = @$"{Console.ReadLine()}";
+
+            CopyAllFiles(inputPath, outputPath);
+        }
+
+        public static void CopyAllFiles(string inputPath, string outputPath)
+        {
+            if (Directory.Exists(outputPath))
+            {
+                Directory.Delete(outputPath, true);
+            }
+            
+            Directory.CreateDirectory(outputPath);
+            
+            string[] files = Directory.GetFiles(inputPath);
+
+            foreach (string file in files)
+            {
+                var fileName = Path.GetFileName(file);
+                var copyDestination = Path.Combine(outputPath, fileName);
+                File.Copy(fileName, copyDestination);
+            }
+        }
+
+    }
+}
