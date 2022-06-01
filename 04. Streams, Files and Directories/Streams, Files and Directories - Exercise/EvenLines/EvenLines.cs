993c9a2131d1e388652e82971bab1e20748d3063
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2022-06-01 13:08:02 +0300

    Add solutions

---
+++ 04. Streams, Files and Directories/Streams, Files and Directories - Exercise/EvenLines/EvenLines.cs
@@ -0,0 +1,53 @@
+﻿namespace EvenLines
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    public class EvenLines
+    {
+        static void Main(string[] args)
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+
+            ProcessLines(inputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath)
+        {
+            StreamReader reader = new StreamReader(inputFilePath);
+            int counter = 0;
+
+            using (reader)
+            {
+                string line;
+                while (!reader.EndOfStream)
+                {
+                    line = reader.ReadLine();
+                    if (counter % 2 == 0)
+                    {
+                        line = Replace(line);
+                        line = Reverse(line);
+                        Console.WriteLine(line);
+                    }
+                    counter++;
+                }
+            }
+
+        }
+
+        private static string Reverse(string line)
+        {
+            line = string.Join(" ", line.Split(' ').Reverse());
+            return line;
+
+        }
+
+        private static string Replace(string line)
+        {
+            return line.Replace("-", "@").Replace(",", "@").Replace(".", "@").Replace("?", "@").Replace("!", "@");
+        }
+
+    }
+
+}
---
+++ 04. Streams, Files and Directories/Streams, Files and Directories - Exercise/LineNumbers/LineNumbers.cs
@@ -0,0 +1,36 @@
+﻿namespace LineNumbers
+{
+    using System;
+    using System.IO;
+
+    public class LineNumbers
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = @"..\..\..\text.txt";
+            string outputPath = @"..\..\..\output.txt";
+
+            ProcessLines(inputPath, outputPath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            StreamReader reader = new StreamReader(inputFilePath);
+            StreamWriter writer = new StreamWriter(outputFilePath);
+
+            using (writer)
+            {
+                using (reader)
+                {
+                    int counter = 1;
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        writer.WriteLine($"{counter}. {line}");
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}
