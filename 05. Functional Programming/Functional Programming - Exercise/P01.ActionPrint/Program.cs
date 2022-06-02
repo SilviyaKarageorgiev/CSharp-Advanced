3d470de7846304097acabcfec84da4c8a96b1f45
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2022-06-02 12:06:03 +0300

    Add solutions from Functional Programming - Exercise

---
+++ 05. Functional Programming/Functional Programming - Exercise/P01.ActionPrint/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01.ActionPrint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> list = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            Action<string> print = name => Console.WriteLine(name);
+            list.ForEach(print);
+
+        }
+    }
+}
---
+++ 05. Functional Programming/Functional Programming - Exercise/P02.KnightsOfHonor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02.KnightsOfHonor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            //Action<string> printSir = name => Console.WriteLine("Sir " + name);
+            //input.ForEach(printSir);
+
+            Func<string, string> returnPlusSir = name => "Sir " + name;
+
+            foreach (var item in input)
+            {
+                Console.WriteLine(returnPlusSir(item));
+            }
+        }
+    }
+}
