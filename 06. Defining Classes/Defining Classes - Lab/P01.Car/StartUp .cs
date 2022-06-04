0328b267118bf23f155cc7883e14f6e521f1d0fc
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2022-06-04 21:57:53 +0300

    Add a solution

---
+++ 06. Defining Classes/Defining Classes - Lab/P01.Car/StartUp .cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            Car car1 = new Car();
+            car1.Make = "Mercedes";
+            car1.Model = "ML";
+            car1.Year = 2004;
+
+            Car car2 = new Car()
+            {
+                Make = "Honda",
+                Model = "Jazz",
+                Year = 2002
+            };
+
+            Console.WriteLine($"Make: {car1.Make}, Model: {car1.Model}, Year: {car1.Year}");
+        }
+    }
+}
