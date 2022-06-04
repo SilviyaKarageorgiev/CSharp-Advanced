530e8d5b9bc70fccf36835b13c3ff1b3da55aab2
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2022-06-04 23:27:41 +0300

    Add another solution

---
+++ 06. Defining Classes/Defining Classes - Lab/P02.CarExtension/Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    internal class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public double FuelQuantity { get; set; }
+
+        public double FuelConsumption { get; set; }
+
+        public void Drive(double distance)
+        {
+            if (this.FuelQuantity - (distance * this.FuelConsumption) > 0)
+            {
+                this.FuelQuantity -= distance * this.FuelConsumption;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make} \nModel: {this.Model} \nYear: {this.Year} \nFuel: {this.FuelQuantity:F2}";
+        }
+    }
+}
---
+++ 06. Defining Classes/Defining Classes - Lab/P02.CarExtension/StartUp .cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            
+            Car car = new Car();
+            car.Make = "Mercedes";
+            car.Model = "GLS";
+            car.Year = 2022;
+            car.FuelQuantity = 80;
+            car.FuelConsumption = 0.8;
+
+            double distance = 20;
+
+            car.Drive(distance);
+            Console.WriteLine(car.WhoAmI());
+        }
+    }
+}
