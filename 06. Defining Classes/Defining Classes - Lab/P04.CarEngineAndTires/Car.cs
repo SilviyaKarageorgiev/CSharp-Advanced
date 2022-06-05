067afe3919ff360817a10a6a756c9e0429bf722d
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2022-06-05 20:06:02 +0300

    Add solutions

---
+++ 06. Defining Classes/Defining Classes - Lab/P04.CarEngineAndTires/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double FuelQuantity { get; set; }
+        public double FuelConsumption { get; set; }
+
+        private Engine engine;
+        private Tire tire;
+
+        public Engine Engine { get; set; }
+        public Tire[] Tires { get; set; }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption, Engine engine, Tire[] tires)
+        {
+            this.Engine = engine;
+            this.Tires = tires;
+        }
+    }
+}
