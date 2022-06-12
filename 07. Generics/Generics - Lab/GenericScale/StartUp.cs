263ba74f0c81f7abd1ef2cd3266687db8b5de39c
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2022-06-12 22:40:48 +0300

    Add one more solution

---
+++ 07. Generics/Generics - Lab/GenericScale/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GenericScale
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            int left = 5;
+            int right = 7;
+
+            EqualityScale<int> equalityScale = new EqualityScale<int>(left, right);
+            Console.WriteLine(equalityScale.AreEqual());
+
+        }
+    }
+}
