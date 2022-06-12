8b24a0164e9be1afd75acf0970dcbf73022618eb
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2022-06-12 21:17:16 +0300

    Add a solution from generics lab

---
+++ 07. Generics/Generics - Lab/P01.Box/BoxOfT.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class BoxOfT<T>
+    {
+        private List<T> internalList = new List<T>();
+
+        public int Count => internalList.Count;
+
+        public void Add(T element)
+        {
+            this.internalList.Add(element);
+        }
+
+        public T Remove()
+        {
+            var element = internalList[internalList.Count - 1];
+            internalList.RemoveAt(internalList.Count - 1);
+            return element;
+        }
+
+    }
+}
---
+++ 07. Generics/Generics - Lab/P01.Box/StartUp .cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BoxOfT
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            BoxOfT<int> box = new BoxOfT<int>();
+            box.Add(1);
+            Console.WriteLine(box.Remove());
+            Console.WriteLine(box.Count);
+
+        }   
+    }
+}
