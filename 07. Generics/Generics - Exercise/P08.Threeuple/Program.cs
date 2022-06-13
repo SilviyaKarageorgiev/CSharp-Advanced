9fef95ee52c4015dba5cf89f75a6b4c90e1fa3ba
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2022-06-13 17:19:11 +0300

    Add a solution

---
+++ 07. Generics/Generics - Exercise/P08.Threeuple/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace P08.Threeuple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input1 = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string name = input1[0] + " " + input1[1];
+            string address = input1[2];
+            string town = input1.Length > 4 ? input1[3] + " " + input1[4] : input1[3];
+            
+
+            Threeuple<string, string, string> personInfo = new Threeuple<string, string, string>(name, address, town);
+
+            Console.WriteLine(personInfo);
+
+            string[] input2 = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string nameDrinker = input2[0];
+            int liters = int.Parse(input2[1]);
+            bool isDrunk = input2[2] == "drunk" ? true : false;
+
+            Threeuple<string, int, bool> drinkerInfo = new Threeuple<string, int, bool>(nameDrinker, liters, isDrunk);
+
+            Console.WriteLine(drinkerInfo);
+
+            string[] input3 = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string nameBankAccount = input3[0];
+            double accountBalance = double.Parse(input3[1]);
+            string bankName = input3[2];
+
+            Threeuple<string, double, string> numbersInfo = new Threeuple<string, double, string>(nameBankAccount, accountBalance, bankName);
+
+            Console.WriteLine(numbersInfo);
+
+        }
+    }
+}
---
+++ 07. Generics/Generics - Exercise/P08.Threeuple/Threeuple.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P08.Threeuple
+{
+    public class Threeuple<T1, T2, T3>
+    {
+        public Threeuple(T1 t1, T2 t2, T3 t3)
+        {
+            Item1 = t1;
+            Item2 = t2;
+            Item3 = t3;
+        }
+
+        public T1 Item1 { get; set; }
+        public T2 Item2 { get; set; }
+        public T3 Item3 { get; set; }
+
+        public override string ToString()
+        => $"{Item1} -> {Item2} -> {Item3}";
+    }
+}
