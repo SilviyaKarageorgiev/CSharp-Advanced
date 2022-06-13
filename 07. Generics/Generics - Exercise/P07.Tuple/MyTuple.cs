014f4a5debafbbc993ecc1ed2a13598e44f10ab8
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2022-06-13 16:44:01 +0300

    Add a solution

---
+++ 07. Generics/Generics - Exercise/P07.Tuple/MyTuple.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P07.Tuple
+{
+    public class MyTuple<T1, T2>
+    {
+        public MyTuple(T1 t1, T2 t2)
+        {
+            Item1 = t1;
+            Item2 = t2;
+        }
+
+        public T1 Item1 { get; set; }
+        public T2 Item2 { get; set; }
+
+        public override string ToString()
+        => $"{Item1} -> {Item2}";
+    }
+}
---
+++ 07. Generics/Generics - Exercise/P07.Tuple/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace P07.Tuple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] input1 = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string name = input1[0] + " " + input1[1];            
+            string town = input1[2];
+
+            MyTuple<string, string> personInfo = new MyTuple<string, string>(name, town);
+
+            Console.WriteLine(personInfo);
+
+            string[] input2 = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string nameDrinker = input2[0];
+            int liters = int.Parse(input2[1]);
+
+            MyTuple<string, int> drinkerInfo = new MyTuple<string, int>(nameDrinker, liters);
+
+            Console.WriteLine(drinkerInfo);
+
+            string[] input3 = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            int num1 = int.Parse(input3[0]);
+            double num2 = double.Parse(input3[1]);
+
+            MyTuple<int, double> numbersInfo = new MyTuple<int, double>(num1, num2);
+
+            Console.WriteLine(numbersInfo);
+        }
+    }
+}
