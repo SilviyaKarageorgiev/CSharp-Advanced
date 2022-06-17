a1a52fcee92d79af4595659b3753179d8aee447c
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2022-06-17 22:05:46 +0300

    Exam preparation

---
+++ Exam Preparation/CSharpAdvancedExam28June2020/P01.BombsCSharpAdvancedExam28June2020/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P01.BombsCSharpAdvancedExam28June2020
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
