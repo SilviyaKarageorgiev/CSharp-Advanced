cc8dbcffe58aa9724f3880d7a99d71b5baf14974
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2022-06-23 13:34:17 +0300

    Add a solution

---
+++ Exam Preparation/CSharpAdvancedExam22Feb2020/Guild/Guild.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Guild
+{
+    public class Guild
+    {
+
+        List<Player> roster;
+        public List<Player> Roster { get; set; }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int Count => Roster.Count;
+
+        public Guild(string name, int capacity)
+        {
+            Roster = new List<Player>();
+            Name = name;
+            Capacity = capacity;
+        }
+
+        public void AddPlayer(Player player)
+        {
+            if (Capacity > 0)
+            {
+                Roster.Add(player);
+                Capacity--;
+            }
+        }
+
+        public bool RemovePlayer(string name)
+        {
+            if (Roster.Any(x => x.Name == name))
+            {
+                Roster.Remove(Roster.First(x => x.Name == name));
+                Capacity++;
+                return true;
+            }
+            return false;
+        }
+
+        public void PromotePlayer(string name)
+        {
+            if (!Roster.Any(x => x.Name == name && x.Rank == "Member"))
+            {
+                var player = Roster.Where(x => x.Name == name).First();
+                player.Rank = "Member";
+            }
+        }
+
+        public void DemotePlayer(string name)
+        {
+            if (!Roster.Any(x => x.Name == name && x.Rank == "Trial"))
+            {
+                var player = Roster.Where(x => x.Name == name).First();
+                player.Rank = "Trial";
+            }
+        }
+
+        public Player[] KickPlayersByClass(string classs)
+        {
+            List<Player> kickedPlayers = new List<Player>();
+            kickedPlayers = Roster.Where(x => x.Class == classs).ToList();
+
+            foreach (var player in kickedPlayers)
+            {
+                Roster.Remove(player);
+                Capacity++;
+            }
+
+            Player[] kicked = new Player[kickedPlayers.Count];
+            kicked = kickedPlayers.ToArray();
+            return kicked;
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Players in the guild: {this.Name}");
+
+            foreach (var item in Roster)
+            {
+                sb.AppendLine(item.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
---
+++ Exam Preparation/CSharpAdvancedExam22Feb2020/Guild/Player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guild
+{
+    public class Player
+    {
+
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public string Rank { get; set; } = "Trial";
+        public string Description { get; set; } = "n/a";
+
+        public Player(string name, string classs)
+        {
+            Name = name;
+            Class = classs;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Player {Name}: {Class}");
+            sb.AppendLine($"Rank: {Rank}");
+            sb.AppendLine($"Description: {Description}");
+
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
