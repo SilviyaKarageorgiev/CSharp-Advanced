31c245213081bef9aedd307b3edc713eca36f8e7
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2022-06-24 00:38:34 +0300

    Add a solution

---
+++ Exam Preparation/CSharpAdvancedRetakeExam14April2021/CocktailParty/Cocktail.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CocktailParty
+{
+    internal class Cocktail
+    {
+
+        List<Ingredient> ingredients;
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int MaxAlcoholLevel { get; set; }
+        public List<Ingredient> Ingredients { get; set; }
+
+        public Cocktail(string name, int capacity, int maxAlcoholLevel)
+        {
+            Ingredients = new List<Ingredient>();
+            Name = name;
+            Capacity = capacity;
+            MaxAlcoholLevel = maxAlcoholLevel;
+        }
+
+        public void Add(Ingredient ingredient)
+        {
+            if (!Ingredients.Any(x => x.Name == ingredient.Name) && Capacity > 0 && ingredient.Alcohol <= MaxAlcoholLevel)
+            {
+                Ingredients.Add(ingredient);
+               
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            if (Ingredients.Any(x => x.Name == name))
+            {
+                Ingredients.Remove(Ingredients.Where(x => x.Name == name).First());
+                return true;
+            }
+            return false;
+        }
+
+        public Ingredient FindIngredient(string name)
+        {
+            return Ingredients.FirstOrDefault(x => x.Name == name);
+        }
+
+        public Ingredient GetMostAlcoholicIngredient()
+        {
+            return Ingredients.OrderByDescending(x => x.Alcohol).First();
+        }
+
+        public int CurrentAlcoholLevel => Ingredients.Sum(i => i.Alcohol);
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Cocktail: {Name} - Current Alcohol Level: {CurrentAlcoholLevel}");
+
+            foreach (var ing in Ingredients)
+            {
+                sb.AppendLine(ing.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
